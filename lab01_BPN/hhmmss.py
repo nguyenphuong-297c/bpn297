a23dd8669332317aa4848c0728595886c309ddb1
nguyenphuong-297c <nguyenbichphuong0104@gmail.com>
2024-08-30 11:25:01 +0700

    Add files via upload

---
+++ lab01_BPN/hhmmss.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 11:22:35 2024
+
+@author: Admin
+"""
+r = int(input("Nhập bán kính đường tròn: "))
+r = int(input("Nhập bán kính đường tròn: "))
+print("Chu vi hình tròn: ",r*2*3.14)
+print("Diện tích hình tròn: ",r*r*3.14)
